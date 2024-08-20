8834666e4df383a2a7f3e79071c9800e9ca216ba
John Ardagelou <j.ardagelou@gmail.com>
2024-08-20 15:03:30 +0300

    First Todo-List App

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function TodoInput(props) {
+    const {handleAddTodos,todoValue, setTodoValue} = props;
+
+  
+
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter To Do..."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+          
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
+}
